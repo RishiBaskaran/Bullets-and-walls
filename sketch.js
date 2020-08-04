9bc204a3ee4b0d8f95928b896fc2dbc42749dd34
RishiBaskaran <68693669+RishiBaskaran@users.noreply.github.com>
2020-08-04 18:20:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,38 @@
-var car,wall;
-
-
+var bullet,wall,thickness;
+var speed,weight;
+var deformation;
 function setup() {
-  createCanvas(800,400);
-  car = createSprite(400, 200, 40, 50);
-  wall = createSprite(200,200,30,40)
-  car.shapeColor = "white";
-  rectangle2.shapeColor = "black";
-  car.velocityX = 3;
+  createCanvas(1600,400);
+  speed = random(223,321);
+  weight = random(30,52);
+ car = createSprite(50, 200, 20, 20);
+  car.shapeColor ="white";
+  thickness = random(22,83);
+ wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = color(80,80,80);
+  
+ 
  // rectangle1.velocityY = -3;
+
 }
 
 function draw() {
-  background(255,255,255);  
- // rectangle2.x = World.mouseX;
-  //rectangle2.y = World.mouseY;
-
-if(car.x - wall.x < car.width/2 + wall.width/2 && 
-  wall.x - car.x < car.width/2 + wall.width/2 &&
-  car.y - wall.y < car.height/2 + wall.height/2 &&
-  wall.y - call.y < car.height/2 + wall.height/2){
-
-car.velocityX = wall.velocityX * (-1);
-}
-
+  background(255,255,255);
+  background("black");
+  
+  car.velocityX = speed;
+  if(wall.x - car.x < (car.width + wall.width)/2){
+    car.velocityX = 0;
+    var deformation = 0.5*weight*speed*speed/22500;
+    if(deformation > 180){
+      wall.shapeColor = color(255,0,0);
+    }
+    if(deformation < 180 && deformation > 100){
+      wall.shapeColor = color(230,230,0);
+    }
+    if(deformation < 100){
+      wall.shapeColor = color(0,255,0);
+    }
+  }
   drawSprites();
 }
