e7b681de38316e2ccbf49210e6abe957f2dd6058
RishiBaskaran <68693669+RishiBaskaran@users.noreply.github.com>
2020-08-05 09:00:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var bullet,wall,thickness;
 var speed,weight;
-var deformation;
+var damage;
 function setup() {
   createCanvas(1600,400);
   speed = random(223,321);
@@ -35,5 +35,29 @@ function draw() {
     }
   }*/
   
+  
+  if(hascollided(bullet,wall))
+  {
+   bullet.velocityX = 0; 
+    
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+    
+    if(damage>10)
+    {
+      wall.shapeColor = color(255,0,0);
+  }
+    if(damage<10)
+    {
+      wall.shapeColor = color(0,255,0);
+  } 
   drawSprites();
+}
+
+function hascollided(lbullet,lwall){
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+  return true
+  }
+  return false;
 }
